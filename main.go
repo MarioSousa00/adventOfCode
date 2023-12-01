8fb1e5a4339b54179449dc1f91dac1650e59ae08
mtwmariosousa <mario.sousa@byside.com>
2023-12-01 22:11:14 +0000

    Main file creation

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"adventOfCode/advent2023"
+)
+
+func main() {
+	var year string = os.Args[1];
+	var day string = os.Args[2];
+	var challenge string = os.Args[3];
+	var inputFilePath string = os.Args[4];
+
+	executeChallenge(year, day, challenge, inputFilePath);
+}
+
+func executeChallenge(year string, day string, challenge string, inputFilePath string) {
+	var challengesMap map[string]func(string) = initChallengesMap();
+
+	var challengeKey = year + "_" + day + "_" + challenge;
+	challengesMap[challengeKey](inputFilePath);
+}
+
+func initChallengesMap() map[string]func(string) {
+	var result map[string]func(string) = make(map[string]func(string));
+
+	result["2023_01_01"] = advent2023.Day01_1;
+	result["2023_01_02"] = advent2023.Day01_2;
+
+	return result;
+}
